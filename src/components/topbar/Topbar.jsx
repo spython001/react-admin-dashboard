042eb2d52c27776287dcceec63c6852b4804791e
spython <emmanuelegbo01@gmail.com>
2023-10-18 11:09:21 +0100

    modified contents of topbar folder component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,27 @@
+import { Language, NotificationsNone, Settings } from '@mui/icons-material'
 import './topbar.css'
 
 export default function Topbar() {
   return (
-    <div>
+    <div className='topbar'>
         <div className="topbarWrapper">
-            <div className="topbarLeft">left</div>
-            <div className="topbarRight">right</div>
+            <div className="topLeft">
+                <span className="logo">Spyderadmin</span>
+            </div>
+            <div className="topRight">
+                <div className="topbarIconContainer">
+                    <NotificationsNone/>
+                    <span className="topIconBadge">2</span>
+                </div>
+                <div className="topbarIconContainer">
+                    <Language/>
+                    <span className="topIconBadge">2</span>
+                </div>
+                <div className="topbarIconContainer">
+                    <Settings/>
+                </div>
+                <img className='topAvatar' src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+            </div>
         </div>
     </div>
   )
