f6f6265e414003b165966691d84be9b1df3a5a46
spython <emmanuelegbo01@gmail.com>
2023-10-18 12:36:44 +0100

    modified contents of featured info folder

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,4 +1,4 @@
-import { ArrowDownward } from '@mui/icons-material'
+import { ArrowDownward, ArrowUpward } from '@mui/icons-material'
 import './featuredinfo.css'
 
 export default function FeaturedInfo() {
@@ -9,7 +9,29 @@ export default function FeaturedInfo() {
             <div className="featuredMoneyContainer">
                 <span className="featuredMoney">$2,415</span>
                 <span className="featuredMoneyRate">
-                    -11.4 <ArrowDownward/>
+                    -11.4 <ArrowDownward className='featuredIcon negative'/>
+                </span>
+            </div>
+            <span className="featuredSub">Compared to last month</span>
+        </div>
+
+        <div className="featuredItem">
+            <span className="featuredTitle">Sales</span>
+            <div className="featuredMoneyContainer">
+                <span className="featuredMoney">$4,415</span>
+                <span className="featuredMoneyRate">
+                    -1.4 <ArrowDownward className='featuredIcon negative'/>
+                </span>
+            </div>
+            <span className="featuredSub">Compared to last month</span>
+        </div>
+
+        <div className="featuredItem">
+            <span className="featuredTitle">Cost</span>
+            <div className="featuredMoneyContainer">
+                <span className="featuredMoney">$3,415</span>
+                <span className="featuredMoneyRate">
+                    +2.4 <ArrowUpward className='featuredIcon'/>
                 </span>
             </div>
             <span className="featuredSub">Compared to last month</span>
