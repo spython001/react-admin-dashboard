ffae444856a34021580a9ea6da0f3059d0395a5e
spython <emmanuelegbo01@gmail.com>
2023-10-19 06:50:25 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,26 @@ import './app.css'
 import Sidebar from "./components/sidebar/Sidebar"
 import Topbar from "./components/topbar/Topbar"
 import Home from './pages/home/Home'
-
+import {BrowserRouter as Router, Switch, Route,} from "react-router-dom"
+import UserList from './pages/userList/UserList'
 
 function App() {
   return (
     <>
-      <div className='app'>
+      <Router>
         <Topbar/>
         <div className="container">
           <Sidebar/>
-          <Home/>
+          <Switch>
+            <Route exact path='/'>
+              <Home/>
+            </Route>
+            <Route path='/users'>
+              <UserList/>
+            </Route>
+          </Switch>
         </div>
-      </div>
+      </Router>
     </>
   )
 }
